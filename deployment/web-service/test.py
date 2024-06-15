08b669621387ce41b92a5848fc32f109acb1ab05
Jack Kamire <jacmire@gmail.com>
2024-06-15 14:43:44 +0300

    mlflow connectino completed

--- deployment/web-service/test.py
+++ deployment/web-service/test.py
@@ -1,12 +1,8 @@
 import requests
 
-ride = {
-    "PULocationID": 10, 
-    "DOLocationID": 50,
-    "trip_distance": 40
-}
+ride = {"PULocationID": 10, "DOLocationID": 50, "trip_distance": 40}
 
-url = 'http://127.0.0.1:9696/predict'
+url = "http://127.0.0.1:9696/predict"
 
 response = requests.post(url, json=ride)
 print(response.json())
